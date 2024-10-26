3878be9b1fea8a126ff72ac4e20d57ffd706f87a
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-26 15:33:55 +0600

    Created a file with array of categories to use it in the components methods

---
+++ src/Helpers/categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+  {title: 'Star Wars', id: 'star-wars'},
+  {title: 'Famous People', id: 'famous-people'},
+  {title: 'Saying', id: 'saying'},
+  {title: 'Humor', id: 'humor'},
+  {title: 'Motivational', id: 'motivational'},
+];
