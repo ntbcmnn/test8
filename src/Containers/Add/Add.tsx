31b2407fa30166899dbd62f86653a9cb1c2a7dc4
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-26 16:13:05 +0600

    Formatted code: wrote code in the "return" in one line

--- src/Containers/Add/Add.tsx
+++ src/Containers/Add/Add.tsx
@@ -23,10 +23,7 @@ const Add = () => {
   };
 
   return <>
-    {loader ?
-      <Loader/>
-      :
-      <QuoteForm submitForm={submitForm}/>}
+    {loader ? <Loader/> : <QuoteForm submitForm={submitForm}/>}
   </>;
 };
 
